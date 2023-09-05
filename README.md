0e4c83bb0ce4f94483464db76e05afb1f9ba9624
formegusto <formegusto@gmail.com>
2023-09-06 04:07:05 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## MongoDB 시계열 데이터를 위한 전용 컬렉션 타입 Time Series Collection
---
+++ bin/init-mongo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+mongosh <<EOF
+
+use $MONGO_INITDB_DATABASE
+
+db.createUser({
+  user:  '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+})
+
+db.createCollection("weather", {
+    timeseries: {
+        timeField: "timestamp",
+        metaField: "metadata"
+    }
+})
+
+EOF
